1cf5b86552208dcca878721db00a72a0c69041d0
Jackson Santos <jacksf1210@gmail.com>
2022-02-26 08:48:12 -0300

    Atualizações de dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
 <Banner />
   <div class="main-container">
     <h1>Monte seu hamburguer</h1>
-  <p>Formulário</p>
+  <BurgerForm />
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue"
+import BurgerForm from '../components/BurgerForm'
 
 export default{
   name: "Home",
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 </script>
